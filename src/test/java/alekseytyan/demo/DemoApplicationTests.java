b5c8ce6fd2b8e1f1934fff3f35d89f4cb574fea7
AlexeyTyan <alekseytyan45@gmail.com>
2021-02-23 15:29:46 +0300

    Initial commit

---
+++ src/test/java/alekseytyan/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package alekseytyan.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

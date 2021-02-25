7e482eee0ff079bbd88e81a772957981587b3c17
AlexeyTyan <alekseytyan45@gmail.com>
2021-02-25 21:23:25 +0300

    Fixed bug (countrySinger bean could not be created because of a typo)

--- src/main/java/alekseytyan/config/AppConfig.java
+++ src/main/java/alekseytyan/config/AppConfig.java
@@ -1,5 +1,6 @@
 package alekseytyan.config;
 
+import alekseytyan.converter.StringToDateTimeConverter;
 import alekseytyan.entity.Singer;
 import org.joda.time.DateTime;
 import org.springframework.beans.factory.annotation.Value;
@@ -7,16 +8,23 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.ConversionServiceFactoryBean;
+import org.springframework.core.convert.converter.Converter;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.HashSet;
+import java.util.Set;
 
 @Configuration
 @EnableTransactionManagement
 @ComponentScan(basePackages = {"alekseytyan.*"})
 @PropertySource("classpath:application.properties")
 public class AppConfig {
+
+    @Value("${date.format.pattern}")
+    private String dateFormatPattern;
 
     @Bean
     public Singer eric() throws MalformedURLException {
@@ -28,20 +36,30 @@ public class AppConfig {
         );
     }
 
-    @Value("${countrySinger.firstName}")
-    private String firstName;
+    @Bean
+    public Singer countrySinger(@Value("${countrySinger.firstName}") String firstName,
+                                @Value("${countrySinger.lastName}") String lastName,
+                                @Value("${countrySinger.birthDate}") DateTime birthdate,
+                                @Value("${countrySinger.personalSite}") URL website) {
+        return new Singer(firstName, lastName, birthdate, website);
+    }
 
-    @Value("${countrySinger.lastName}")
-    private String lastName;
-
-    @Value("${countrySinger.birthdate}")
-    private DateTime birthdate;
-
-    @Value("${countrySinger.personalSite}")
-    private URL website;
 
     @Bean
-    public Singer countrySinger() {
-        return new Singer(firstName, lastName, birthdate, website);
+    public ConversionServiceFactoryBean conversionService() {
+        ConversionServiceFactoryBean conversionServiceFactoryBean = new ConversionServiceFactoryBean();
+        Set<Converter> convs = new HashSet<>();
+        convs.add(converter());
+        conversionServiceFactoryBean.setConverters(convs);
+        conversionServiceFactoryBean.afterPropertiesSet();
+        return conversionServiceFactoryBean;
+    }
+
+    @Bean
+    public StringToDateTimeConverter converter() {
+        StringToDateTimeConverter converter = new StringToDateTimeConverter();
+        converter.setDatePattern(dateFormatPattern);
+        converter.init();
+        return converter;
     }
 }

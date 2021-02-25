957c52191a21169e9b1c4252fa8b2f148c7d24b9
AlexeyTyan <alekseytyan45@gmail.com>
2021-02-25 15:50:07 +0300

    Implement converter class (String -> DateTime)

---
+++ src/main/java/alekseytyan/converter/StringToDateTimeConverter.java
@@ -0,0 +1,34 @@
+package alekseytyan.converter;
+
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+import org.springframework.core.convert.converter.Converter;
+
+import javax.annotation.PostConstruct;
+
+public class StringToDateTimeConverter implements Converter<String, DateTime> {
+
+    private static final String DEFAULT_DATE_PATTERN = "yyyy-MM-dd";
+
+    private DateTimeFormatter dateFormat;
+    private String datePattern = DEFAULT_DATE_PATTERN;
+
+    public String getDatePattern() {
+        return datePattern;
+    }
+
+    public void setDatePattern(String datePattern) {
+        this.datePattern = datePattern;
+    }
+
+    @Override
+    public DateTime convert(String s) {
+        return dateFormat.parseDateTime(s);
+    }
+
+    @PostConstruct
+    public void init() {
+        dateFormat = DateTimeFormat.forPattern(datePattern);
+    }
+}

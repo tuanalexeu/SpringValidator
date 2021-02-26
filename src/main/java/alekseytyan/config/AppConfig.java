6dde808018b2f2e755ed7f7b9f7fbc0e75bc6c5e
AlexeyTyan <alekseytyan45@gmail.com>
2021-02-26 13:42:54 +0300

    Implement ConversionServiceFactoryBean

--- src/main/java/alekseytyan/config/AppConfig.java
+++ src/main/java/alekseytyan/config/AppConfig.java
@@ -1,9 +1,11 @@
 package alekseytyan.config;
 
+import alekseytyan.converter.ApplicationConversionServiceFactoryBean;
 import alekseytyan.converter.SingerToAnotherSingerConverter;
 import alekseytyan.converter.StringToDateTimeConverter;
 import alekseytyan.entity.Singer;
 import org.joda.time.DateTime;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -15,7 +17,9 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.text.ParseException;
 import java.util.HashSet;
+import java.util.Locale;
 import java.util.Set;
 
 @Configuration
@@ -26,6 +30,13 @@ public class AppConfig {
 
     @Value("${date.format.pattern}")
     private String dateFormatPattern;
+
+    private ApplicationConversionServiceFactoryBean conversionService;
+
+    @Autowired
+    public void setConversionService(ApplicationConversionServiceFactoryBean conversionService) {
+        this.conversionService = conversionService;
+    }
 
     @Bean
     public Singer eric() throws MalformedURLException {
@@ -68,5 +79,16 @@ public class AppConfig {
     @Bean
     public SingerToAnotherSingerConverter singerConverter() {
         return new SingerToAnotherSingerConverter();
+    }
+
+
+    @Bean
+    public Singer john() throws MalformedURLException, ParseException {
+        return new Singer(
+                "John",
+                "Johnny",
+                conversionService.getTimeFormatter().parse("1977-10-16", Locale.ENGLISH),
+                new URL("http://example.com")
+        );
     }
 }
---
+++ src/main/java/alekseytyan/converter/ApplicationConversionServiceFactoryBean.java
@@ -0,0 +1,61 @@
+package alekseytyan.converter;
+
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.Formatter;
+import org.springframework.format.support.FormattingConversionServiceFactoryBean;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.text.ParseException;
+import java.util.HashSet;
+import java.util.Locale;
+import java.util.Set;
+
+@Component
+public class ApplicationConversionServiceFactoryBean extends FormattingConversionServiceFactoryBean {
+
+    private static final String DEFAULT_DATE_PATTERN = "yyyy-MM-dd";
+
+    private DateTimeFormatter dateFormat;
+    private String datePattern = DEFAULT_DATE_PATTERN;
+
+    private final Set<Formatter<?>> formatters = new HashSet<>();
+
+
+    public String getDatePattern() {
+        return datePattern;
+    }
+
+    @Autowired(required = false)
+    public void setDatePattern(String datePattern) {
+        this.datePattern = datePattern;
+    }
+
+
+
+    @PostConstruct
+    public void init() {
+        dateFormat = DateTimeFormat.forPattern(datePattern);
+        formatters.add(getTimeFormatter());
+        setFormatters(formatters);
+    }
+
+    public Formatter<DateTime> getTimeFormatter() {
+        return new Formatter<DateTime>() {
+            @Override
+            public DateTime parse(String s, Locale locale) throws ParseException {
+                return dateFormat.parseDateTime(s);
+            }
+
+            @Override
+            public String print(DateTime dateTime, Locale locale) {
+                return dateFormat.print(dateTime);
+            }
+        };
+    }
+
+
+}
